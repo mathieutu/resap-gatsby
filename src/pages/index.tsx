913d0173767135d650fe18f1b0f4bd3aaa6e75dd
Mathieu TUDISCO <mathieu@tudisco.fr>
2021-05-18 02:03:08 +0200

    🔧 Add TailwindCss.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,33 @@
 import { graphql, PageProps } from 'gatsby'
 import { Fragment } from 'react'
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
+import { TestMenu } from '../components/TestMenu'
 
 // markup
 const IndexPage = ({ data }: PageProps<any>) => {
   return (
-    <div>
-      {data.fiches.nodes.map(fiche => {
-          let contenu = documentToHtmlString(JSON.parse(fiche.contenu.raw))
+    <div
+      className="my-8 max-w-prose mx-auto px-2 md:px-0"
+    >
+      <TestMenu />
+      <div className="mt-8 prose">
+        {data.fiches.nodes.map(fiche => {
+          const contenu = documentToHtmlString(JSON.parse(fiche.contenu.raw))
 
+          console.log(fiche)
           return (
             <Fragment key={fiche.slug}>
-                <h1>{console.log(fiche) || fiche.titre}</h1>
-                <pre>{contenu}</pre>
-                <div dangerouslySetInnerHTML={{ __html: contenu }} />
-                <pre>{JSON.stringify(fiche, null, 2)}</pre>
+              <h1>{fiche.titre}</h1>
+              <pre>{contenu}</pre>
+              <div
+                className="border-green-800 border p-2 bg-green-50"
+                dangerouslySetInnerHTML={{ __html: contenu }}
+              />
+              <pre>{JSON.stringify(fiche, null, 2)}</pre>
             </Fragment>
 
           )
-      })}
+        })}</div>
     </div>
   )
 }
